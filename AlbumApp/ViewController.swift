6bacf66f35d4d0894df67c9e64a1baf72683c649
김승헌 <skrina.dev@gmail.com>
2023-02-04 23:46:45 +0900

    Initial Commit

---
+++ AlbumApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlbumApp
+//
+//  Created by 김승헌 on 2023/02/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
